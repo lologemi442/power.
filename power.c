9c3de1da02c9e110458f9f53610303e6eaccadb9
lologemi442 <36268013+lologemi442@users.noreply.github.com>
2018-03-28 19:30:58 +0200

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <math.h>
+#include <conio.h>
+void main ()
+{
+ int x, y, P ;
+ cout << " Enter a number: " ;
+ cin >> x ;
+ cout << " Enter a power : " ;
+ cin >> y ;
+ P = pow (x,y) ;
+ cout << " The result is : " << P << endl ;
+getch ();
+}
